d428c969be9eeab03802faea09f5be3a4158d116
Fernanda Bonfat <fernandabonfat@hotmail.com>
2024-08-27 12:30:56 -0300

    modifying AccessBrowser class

--- src/test/java/br/com/alura/leilao/AccessBrowser.java
+++ src/test/java/br/com/alura/leilao/AccessBrowser.java
@@ -5,12 +5,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HelloWorldSelenium {
-
-    @Test
+    
     public void hello() {
         System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
         WebDriver browser = new ChromeDriver();
         browser.navigate().to("http://localhost:8080/leiloes");
-        browser.quit();
     }
 }
